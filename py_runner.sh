67ebe12570d39facd9faab4b1f7c81d46e6018dd
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-04-06 02:47:36 -0400

    Update py_runner.sh

--- py_runner.sh
+++ py_runner.sh
@@ -13,9 +13,9 @@ ${THIS_DIR}/bin/hdfs dfs -put ${THIS_DIR}/py/${SUB_DIR}sample/* ${THIS_DIR}/samp
 ${THIS_DIR}/bin/hadoop jar \
     ${THIS_DIR}/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar \
     -file ${THIS_DIR}/py/${SUB_DIR}mapper.py \
-    -mapper ${THIS_DIR}/py/${SUB_DIR}mapper.py \
+    -mapper "${THIS_DIR}/py/${SUB_DIR}mapper.py ${*:2}" \
     -file ${THIS_DIR}/py/${SUB_DIR}reducer.py	\
-    -reducer ${THIS_DIR}/py/${SUB_DIR}reducer.py \
+    -reducer "${THIS_DIR}/py/${SUB_DIR}reducer.py ${*:2}" \
     -input ${THIS_DIR}/sample/* \
     -output ${THIS_DIR}/py-output
 ${THIS_DIR}/bin/hdfs dfs -cat ${THIS_DIR}/py-output/*
