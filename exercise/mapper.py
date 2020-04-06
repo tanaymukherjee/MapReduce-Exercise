15a63287557b774a63e9a19f651a43668a58c98e
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-04-06 06:38:02 -0400

    Add files via upload

---
+++ exercise/mapper.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+# to allow reading of regex expressions
+import re
+
+regex = sys.argv[-1]
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        if re.search(regex, word):
+            print '%s\t%s' % (word, 1)
+        else:
+            print '%s\t%s' % (word, 0)
